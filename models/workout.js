b1856c2744ccb14ba8d9fe695db3258d84ab7a62
Chao Ly <lychaos123@gmail.com>
2021-08-04 21:17:34 -0500

    added the exercise with name, type, trim, and required. Having error with the exercise area.

--- models/workout.js
+++ models/workout.js
@@ -5,9 +5,34 @@ const workoutSchema = mongoose.Schema;
 const workoutSchema = new Schema({
     day: {
         type: Date,
-        default: () => 
-    }
-})
+        default: Date.now
+    },
+    exercises: {[
+        type: {
+            type: String,
+            trim: true,
+            required: "Enter the type of exercise."
+        },
+        name: {
+            type: String,
+            trim: true,
+            required: "Name of the exercise."
+        },
+        weight: {
+            type: INTEGER
+        },
+        sets: {
+            type: INTEGER
+        },
+        reps{
+            type: INTEGER
+        },
+        duration: {
+            type: INTEGER,
+            required: "Time of the exercise."
+        }
+    ]};
+});
 
 
 module.exports = workout;
