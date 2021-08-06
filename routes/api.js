bd4834be6752e1ee280f60fa21c59200924808fc
Chao Ly <lychaos123@gmail.com>
2021-08-05 22:33:34 -0500

    copying from index.js and paste it to api.js since I code into the wrong file.

---
+++ routes/api.js
@@ -0,0 +1,39 @@
+const router = require('express').Router();
+const workOut = require('../models/workout');
+
+router.post('/api/workout', ({ body }, req, res) => {
+    workOut.create(body)
+    .then(dbworkOut => {
+        res.json(dbworkOut);
+    })
+    .catch(err => {
+        res.status(400);
+        console.log(err)
+    });
+});
+
+router.get('./api/workout/range', (req.then, res) => {
+    workOut.find({})
+    .then(dbworkOut => {
+        res.json(dbworkOut)
+    })
+    .catch(err => {
+        res.json(400);
+        console.log(err)
+    })
+});
+
+
+router.put('/api/workouts/:id', ({ body, params }, req, res) => {
+    workOut.findOneAndUpdate(
+        { _id: params.id },
+        { $push: { exerices: body } },
+        { new: true })
+        .then(dbworkOut => {
+            res.json(dbworkOut);
+        })
+        .catch(err => {
+            res.json(400);
+            console.log(err)
+        });
+});
