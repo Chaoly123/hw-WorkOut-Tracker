1e3320d898dae0cd3c511dc273c80dc06503d9ee
Chao Ly <lychaos123@gmail.com>
2021-08-05 21:34:03 -0500

    add coding line 6 and below will app.listen.

--- server.js
+++ server.js
@@ -3,3 +3,20 @@ const mongoose =  require("mongoose");
 
 const PORT = process.env.PORT || 3001
 
+const app = express();
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+app.use(express.static("public"));
+
+const config = { userNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedToplogy: true,
+    useFindAndModify: false
+}
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', config);
+
+app.listen(PORT, () => {
+    
+})
