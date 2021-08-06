6afee3608a7c840eadfa0241bdaab701ede6bd60
Chao Ly <lychaos123@gmail.com>
2021-08-05 23:05:30 -0500

    added router.get for index.html, stats.html, and exercise.html.

---
+++ routes/index.js
@@ -0,0 +1,31 @@
+const router = require('express').Router();
+const path = require('path');
+
+router.get('./', (req, res) => {
+    try {
+        res.sendFile(path.join(_dirname, '../public/index.html'))
+    } catch (err) {
+        res.status(400);
+        console.log(err)
+    }
+});
+
+router.get('./stats', (req, res) => {
+    try {
+        res.sendFile(path.join(_dirname, '../public/stats.html'))
+    } catch (err) {
+        res.status(400);
+        console.log(err)
+    }
+});
+
+router.get('./exercise', (req, res) => {
+    try {
+        res.sendFile(path.join(_dirname, '../public/exercise.html'))
+    } catch (err) {
+        res.status(400);
+        console.log(err)
+    }
+});
+
+module.exports = rounter;
